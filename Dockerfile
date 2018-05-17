5df65082d50af1c84143e897a6b054090bd842ca
Steven Yang <yangzhaofengsteven@gmail.com>
2018-05-17 19:21:54 +0800

    update files

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,14 @@ MAINTAINER Steven Yang <yangzhaofengsteven@gmail.com>
 
 ENV DEBIAN_FRONTEND noninteractive
 RUN apt-get update && apt-get install -y pptpd iptables
+RUN rm /etc/pptpd.conf && rm -rf /etc/ppp/ && mkdir /etc/ppp/
 
 COPY ./etc/pptpd.conf /etc/pptpd.conf
 COPY ./etc/ppp/pptpd-options /etc/ppp/pptpd-options
 COPY ./etc/ppp/chap-secrets /etc/ppp/chap-secrets
 
-#COPY entrypoint.sh /entrypoint.sh
-#RUN chmod 0700 /entrypoint.sh
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod 0700 /entrypoint.sh
 
-#ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["/entrypoint.sh"]
 CMD ["pptpd", "--fg"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ set -e
 sysctl -w net.ipv4.ip_forward=1
 
 # configure firewall
-iptables -t nat -A POSTROUTING -s 10.250.80.0/24 ! -d 10.250.80.0/24 -j MASQUERADE
-iptables -A FORWARD -s 10.250.80.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -t nat -A POSTROUTING -s {{LOCAL_SUBNET}}.0/24 ! -d {{LOCAL_SUBNET}}.0/24 -j MASQUERADE
+iptables -A FORWARD -s {{LOCAL_SUBNET}}.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 iptables -A INPUT -i ppp0 -j ACCEPT
 iptables -A OUTPUT -o ppp0 -j ACCEPT
 iptables -A FORWARD -i ppp0 -j ACCEPT
